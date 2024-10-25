fdade60501b1c4ee00ab9d1d37ba9fc16e876376
Robby van Zweeden <rvzweeden@timesaversint.com>
2024-10-25 11:39:48 +0200

    Extract duplicate code

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -15,30 +15,43 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 #[Route('/', name: 'default_')]
 class DefaultController extends AbstractController
 {
+    public function __construct(private readonly QueryHelper $queryHelper, private readonly GraphQLClient $client)
+    {
+    }
+
     /**
      * @throws Exception|TransportExceptionInterface
      */
     #[Route('/', name: 'index')]
-    public function index(GraphQLClient $client, QueryHelper $queryHelper): Response
+    public function index(): Response
     {
         $characterId = Random::getRandomNumber(1, 826);
-        $query = $queryHelper->getCharacterInfo($characterId);
 
-        $info = $client->request('https://rickandmortyapi.com/graphql', $query, 'charactersByIds');
-        $data = current($info);
+        return $this->render(
+            'default/index.html.twig',
+            $this->getCharacterData($characterId)
+        );
+    }
 
-        if (!$data) {
+    /**
+     * @throws Exception|TransportExceptionInterface
+     */
+    #[Route('/character/{characterId}', name: 'character')]
+    public function character(string $characterId): Response
+    {
+        $characterId = (int) $characterId;
+        if (($characterId < 1) || ($characterId > 826)) {
             throw $this->createNotFoundException();
         }
 
         return $this->render(
             'default/index.html.twig',
-            $data
+            $this->getCharacterData($characterId)
         );
     }
 
     /**
-     * @throws Exception
+     * @throws Exception|TransportExceptionInterface
      */
     #[Route('/show/{type}/{query}', name: 'show')]
     public function show(string $type, string $query, DataCollector $collector): Response
@@ -57,5 +70,24 @@ class DefaultController extends AbstractController
                 'results' => $characterCollection->getData(),
             ]
         );
+    }
+
+    /**
+     * @param int $characterId
+     * @return array<string, array<int|string, mixed>>
+     * @throws TransportExceptionInterface
+     */
+    private function getCharacterData(int $characterId): array
+    {
+        $query = $this->queryHelper->getCharacterInfo($characterId);
+
+        $info = $this->client->request('https://rickandmortyapi.com/graphql', $query, 'charactersByIds');
+        $data = current($info);
+
+        if (!$data) {
+            throw $this->createNotFoundException();
+        }
+
+        return $data;
     }
 }
